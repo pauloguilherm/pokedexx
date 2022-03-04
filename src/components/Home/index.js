3add7b456f8abd64505a2800ee0d9109822f7e8f
PauloGuilherm <paulo.guilherme.arruda1@gmail.com>
2022-03-03 22:12:21 -0300

    exibindo cards com dados dos pokemons e estilizando eles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,79 @@
+import './home.css'
+import { MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import { useEffect, useState } from 'react';
-import api from '../../service/api'
+import api from '../../service/api';
+
 
 
 export default function Home(){
 
-    const [pokemons, setPokemons] = useState([])
+    const [pokemonName, setPokemonName] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [infos, setInfos] = useState([])
+    const infosPokes = []
 
-    useEffect(() =>{
 
-      async function loadPokemons(){
+    useEffect(()=>{
 
-        const response = await api.get('bulbasaur');
+        async function loadPokemons(){
 
-        setPokemons(response.data)
-        console.log(pokemons)
-      }
+            const pokemons = await api.get()
+            setPokemonName(pokemons.data.results) 
+ 
+          }
+
+        async function dataPokemons(){
+            for(let i = 0;  i < pokemonName.length; i++){
+                
+                infosPokes[i] = await api.get(pokemonName[i].name)
+                .then((res)=>{
+                   return res.data
+                })
+                
+            }        
+            setInfos(infosPokes)
+            setInterval(()=>{
+                setLoading(false)
+            },1000)
+            
+        }
+        
 
        loadPokemons();
-    }, [])
+       dataPokemons();
 
-    return(
-        <div className="home">
-        </div>
-    )
+       
+    }, [loading])
+
+
+     if(loading){
+        return(
+            <h1>Carregando...</h1> 
+        )
+     }
+
+     else{console.log(infos)
+        
+        return(
+            
+            <div className="container-pokes">
+                {infos.map((item, key)=>{
+
+                    return(
+                        <div key={key}>
+                            <div className="cards">
+                               <img className="pokesImg" src={item.sprites.other.dream_world.front_default}/>
+                               <p>Nº00{item.id}</p>
+                               <h1>{item.name}</h1>
+                               <h4>Types: {item.types.length > 1 ? 
+                               <><span>{item.types[0].type.name}</span>
+                               <span>{item.types[1].type.name}</span></>
+                               :<span>{item.types[0].type.name}</span>}</h4>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+     }
 }
